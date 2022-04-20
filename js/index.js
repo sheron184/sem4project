bd4ee40b1986aea340c62209b15a6c0d3062293e
Sheron Jude <53206494+sheron184@users.noreply.github.com>
2022-04-20 12:39:04 -0700

    #2

--- js/index.js
+++ js/index.js
@@ -30,4 +30,22 @@ function pageMove(from,to){
 
   $(from_id).addClass("hide");
   $(to_id).removeClass("hide");
+
+  if(to == 3 || to == 8 || to == 9 || to == 10){
+    $(".page").each(function(){
+      if(!$(this).hasClass("hide")){
+        var footer = $(this).find(".footer");
+        var footer_nav = $(footer).find(".footer-nav");
+        var footer_nav_btns = $(footer_nav).find(".footer-nav-btns");
+        var btns = $(footer_nav_btns).find(".tab");
+        //console.log(btns);
+        $(btns).each(function(){
+          //console.log($(this).attr("id"));
+          if($(this).hasClass(to)){
+            $(this).css("background","#034803");
+          }
+        });
+      }
+    });
+  }
 }
